c341e82e3e59d389e6143f213369362c1998cd0e
Sven Haberer <schroenser@users.noreply.github.com>
2023-03-11 14:41:53 +0100

    Added script to open Tumblr blogs in external viewr.

---
+++ tumblr_open_blogs_in_external_viewer.user.js
@@ -0,0 +1,42 @@
+// ==UserScript==
+// @name         Open Tumblr blog links in external viewer
+// @namespace    https://www.tumblr.com/
+// @version      1.0
+// @description  This will rewrite blog links on Tumblr to be opened in an external viewer.
+// @author       Sven Haberer
+// @match        https://www.tumblr.com/dashboard
+// ==/UserScript==
+
+function replaceHrefs(nodes) {
+    nodes.filter(node => node.hasAttribute("data-id"))
+        .map(node => node.querySelector("div > div > article"))
+        .flatMap(node => {
+            return [
+                "header > div > div.ZJdm4 > div.ffqNn > div > div.vGkyT > span > span > a",
+                "header > div > div.ZJdm4 > div.ffqNn > div > div.jOhmG > div.eLzSX > span > span > a",
+                "div.LaNUG > div > div > span > div > div.fAAi8 > div.QkCNg > div.GdjMk > div > div > span > span > a"]
+                .map(selector => node.querySelector(selector))
+                .filter(node => node);
+        })
+        .forEach(a => {
+            a.href = "https://cascadr.co/blogs/" + a.innerText;
+            a.addEventListener("click", e => e.stopPropagation(), { capture: true });
+        });
+}
+
+var target = document.querySelector("#base-container > div.D5eCV > div.gPQR5 > div.lSyOz > main > div.j8ha0 > div:nth-child(2)");
+
+var config = { childList: true }
+
+var observer = new MutationObserver(function (mutations) {
+    var nodes = mutations
+        .map(mutationRecord => mutationRecord.addedNodes)
+        .flatMap(nodeList => Array.from(nodeList));
+    replaceHrefs(nodes);
+});
+
+observer.observe(target, config);
+
+replaceHrefs(Array.from(target.childNodes));
+
+document.addEventListener("mouseout", e => e.stopPropagation(), { capture: true });
