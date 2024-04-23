5ce879720ab1c5ccd2366292c12525cd8c4f14ba
Sven Haberer <schroenser@users.noreply.github.com>
2024-04-23 08:19:42 +0200

    Added jira-comment-edit-dialog-fix.user.js

---
+++ jira-comment-edit-dialog-fix.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         Jira comment edit dialog fix
+// @description  Makes the edit dialog for comments in Jira more usable.
+// @namespace    https://github.com/schroenser/
+// @version      0.1
+// @match        https://jira.eurodata.de/browse/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=eurodata.de
+// @grant        GM_addStyle
+// @updateURL    https://github.com/schroenser/userscripts/raw/main/jira-comment-edit-dialog-fix.user.js
+// @downloadURL  https://github.com/schroenser/userscripts/raw/main/jira-comment-edit-dialog-fix.user.js
+// ==/UserScript==
+
+GM_addStyle(`
+`);
