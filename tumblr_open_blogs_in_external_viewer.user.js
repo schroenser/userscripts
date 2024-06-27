acbe2787b40723ef2e4644a0585a99c6d77a48bb
Sven Haberer <schroenser@users.noreply.github.com>
2024-06-27 18:24:00 +0200

    Further fixes, stabilizations and corrections.

--- tumblr_open_blogs_in_external_viewer.user.js
+++ tumblr_open_blogs_in_external_viewer.user.js
@@ -1,33 +1,36 @@
 // ==UserScript==
 // @name         Open Tumblr blog links in external viewer
 // @namespace    https://www.tumblr.com/
-// @version      1.2
+// @version      1.3
 // @description  This will rewrite blog links on Tumblr to be opened in an external viewer.
 // @author       Sven Haberer
 // @match        https://www.tumblr.com/*
 // ==/UserScript==
 
 document.addEventListener("mouseout", e => e.stopPropagation(), { capture: true });
-
-var selectors = "#base-container > div.D5eCV > div > div._3xgk.ZN00W > div > div.lSyOz.t8f_N > main > div.Evcyl > div.zAlrA";
+var selectors;
 var hrefFinder;
 
 if (window.location.pathname.startsWith("/dashboard")) {
+    selectors = "#base-container > div.D5eCV > div > div._3xgk.ZN00W > div > div.lSyOz.t8f_N > main > div.Evcyl > div.zAlrA";
     hrefFinder = nodes => nodes
+        .filter(node => node != null)
+        .filter(node => node.querySelector != null)
         .map(node => node.querySelector("div > div > div > article"))
-        .filter(node => node !== null)
+        .filter(node => node != null)
         .flatMap(node =>
                  [
-        "header > div.q4Pce.J_Wh8 > div > div.ffqNn > div > span > span > a",
+        "header > div.q4Pce.J_Wh8 > div > div.ffqNn > div > span.W9hfZ > span > a",
         "header > div.q4Pce.J_Wh8 > div > div.ffqNn > div > div > span > span > a",
         "div.LaNUG > div > div > span > div > div.fAAi8.jLBd9 > div.QkCNg > div.GdjMk > div > div > span > span > span > a",
     ]
                  .map(selector => node.querySelector(selector))
-                 .filter(node => node));
+                 .filter(node => node != null));
 } else if (window.location.pathname == "/following") {
+    selectors = "#base-container > div.D5eCV > div > div._3xgk.ZN00W > div > div.lSyOz.t8f_N > main > section > div.zAlrA";
     hrefFinder = nodes => nodes.flatMap(node => [
-        "a",
-        "div.wmRou > div > a"
+        "div > a",
+        "div > div.wmRou > div > a"
     ]
                                         .map(selector => node.querySelector(selector))
                                         .filter(node => node));
@@ -41,7 +44,8 @@ function replaceHrefs(nodes) {
         var match = [...a.href.matchAll(tumblrUrls)];
         if (match[0]) {
             var blogName = match[0][1] ?? match[0][2]?? match[0][3];
-            a.href = "https://cascadr.co/blogs/" + blogName;
+            var newHref = "https://cascadr.co/blogs/" + blogName;
+            a.setAttribute('href', newHref);
             //a.href = "https://www.tumbex.com/" + blogName + ".tumblr";
             a.addEventListener("click", e => e.stopPropagation(), { capture: true });
         }
