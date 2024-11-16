2c215ac2dce123de8c8cb2a659dda67053f2e688
Sven Haberer <schroenser@users.noreply.github.com>
2024-11-16 12:01:45 +0100

    Updated selectors.

--- tumblr_open_blogs_in_external_viewer.user.js
+++ tumblr_open_blogs_in_external_viewer.user.js
@@ -22,14 +22,14 @@ function replaceHref(a) {
 var selectors;
 if (window.location.pathname.startsWith("/dashboard")) {
     selectors = [
-        "#base-container > div.D5eCV > div > div._3xgk.ZN00W > div > div.lSyOz.t8f_N > main > div.Evcyl > div.zAlrA > div > div > div > div > article > header > div.q4Pce.J_Wh8 > div > div.ffqNn > div > span.W9hfZ > span > a",
-        "#base-container > div.D5eCV > div > div._3xgk.ZN00W > div > div.lSyOz.t8f_N > main > div.Evcyl > div.zAlrA > div > div > div > div > article > header > div.q4Pce.J_Wh8 > div > div.ffqNn > div > div > span > span > a",
-        "#base-container > div.D5eCV > div > div._3xgk.ZN00W > div > div.lSyOz.t8f_N > main > div.Evcyl > div.zAlrA > div > div > div > div > article > div.LaNUG > div > div > span > div > div.fAAi8.jLBd9 > div.QkCNg > div.GdjMk > div > div > span > span > span > a"
+        "#base-container > div.D5eCV > div > div._3xgk.Ril26 > div > div.lSyOz > main > div.Evcyl > div.zAlrA > div > div > div > div > article > header > div.q4Pce.J_Wh8 > div > div.ffqNn > div > span > span > a,
+        "#base-container > div.D5eCV > div > div._3xgk.Ril26 > div > div.lSyOz > main > div.Evcyl > div.zAlrA > div > div > div > div > article > header > div.q4Pce.J_Wh8 > div > div.ffqNn > div > div > span > span > a",
+        "#base-container > div.D5eCV > div > div._3xgk.Ril26 > div > div.lSyOz > main > div.Evcyl > div.zAlrA > div > div > div > div > article > div.LaNUG > div > div > span > div > div.fAAi8.jLBd9 > div.QkCNg > div.GdjMk > div.lBFTL > div > span > span > span > a"
     ];
 } else if (window.location.pathname == "/following") {
     selectors = [
-        "#base-container > div.D5eCV > div > div._3xgk.ZN00W > div > div.lSyOz.t8f_N > main > section > div.zAlrA > div > div > a",
-        "#base-container > div.D5eCV > div > div._3xgk.ZN00W > div > div.lSyOz.t8f_N > main > section > div.zAlrA > div > div > div.wmRou > div > a"
+        "#base-container > div.D5eCV > div > div._3xgk.Ril26 > div > div.lSyOz > main > section > div.zAlrA > div > div > a",
+        "#base-container > div.D5eCV > div > div._3xgk.Ril26 > div > div.lSyOz > main > section > div.zAlrA > div > div > div.wmRou > div > a"
     ];
 }
 
